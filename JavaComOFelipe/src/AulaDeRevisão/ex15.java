43d690a7e77919d04dd53f60faa43be71a64d96a
Luiz Felipe Pires Vieira <91069289+ModiModinha@users.noreply.github.com>
2025-02-12 16:49:52 -0300

    Update ex15.java

--- JavaComOFelipe/src/AulaDeRevisão/ex15.java
+++ JavaComOFelipe/src/AulaDeRevisão/ex15.java
@@ -21,11 +21,16 @@ public class ex15 {
         System.out.println("Digite para qual cidade será a viagem:");
         cidade = sc.nextLine();
 
-
+        System.out.println("---------------------Relatório----------------------");
+        System.out.println("Destino: " + cidade);
+        System.out.println("Consumo do veiculo: " + consumo);
+        System.out.println("Distancia da viagem ida e volta: " + distanciaIeV);
+        System.out.println("Preço dos combustivel: " + preco);
+        System.out.printf("Sua viagem para " + cidade + " tera o gasto de: " + viagem(distanciaIeV, consumo, preco) + " litros");
         System.out.printf("Sua viagem para " + cidade + " tera o gasto de: " + viagem(distanciaIeV, consumo, preco) + " litros");
 
     }
-    public static double viagem(double a , double b, double c) {
-        return (a / b) * c;
+    public static double viagem(double preco , double distanciaIeV, double consumo) {
+        return (distanciaIeV / Consumo) * preco;
     }
 }
